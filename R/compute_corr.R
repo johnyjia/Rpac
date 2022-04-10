f6f68c5b1ebb6d75f6b9e652b37484de09f1c399
John <dr.y.jia@gmail.com>
2022-04-10 15:16:41 -0400

    final push

--- R/compute_corr.R
+++ R/compute_corr.R
@@ -16,7 +16,7 @@
 compute_corr <- function(data, var1, var2){
 
   # compute correlation ----
-  stat::cor.test(
+  stats::cor.test(
     x = data %>% dplyr::pull({{var1}}),
     y = data %>% dplyr::pull({{var2}})
   ) %>%
