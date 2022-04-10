7ba592f6c6dd0f1efbf941ed04aadf23b6cc69ec
John <dr.y.jia@gmail.com>
2022-04-10 15:11:38 -0400

    2push

--- R/compute_corr.R
+++ R/compute_corr.R
@@ -1,7 +1,22 @@
+#' Title
+#'
+#' @param data input dataset
+#' @param var1 variable 1
+#' @param var2 bariable 2
+#'
+#' @return the tidy correlation
+#' @export
+#'
+#' @examples
+#' compute_corr(data = faithful, var1 = eruptions, var2 = waiting)
+#'
+#' @importFrom rlang .data
+#'
+#'
 compute_corr <- function(data, var1, var2){
 
   # compute correlation ----
-  cor.test(
+  stat::cor.test(
     x = data %>% dplyr::pull({{var1}}),
     y = data %>% dplyr::pull({{var2}})
   ) %>%
@@ -9,8 +24,8 @@ compute_corr <- function(data, var1, var2){
   broom::tidy() %>%
     # retain and rename relevant bits ----
   dplyr::select(
-    correlation = estimate,
-    pval = p.value
+    correlation = .data$estimate,
+    pval = .data$p.value
   )
 
 }
